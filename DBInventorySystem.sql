e6b704ec83ac0ca065efd6f270557f9b5d21a2ab
Alexander Castano <42116081+AlexCasdu@users.noreply.github.com>
2024-12-03 08:08:54 -0500

    Update DBInventorySystem.sql

--- DBInventorySystem.sql
+++ DBInventorySystem.sql
@@ -1,9 +1,33 @@
---tabla producto script:
+CREATE TABLE categorias (
+	codigocategoria varchar NOT NULL PRIMARY KEY,
+	categoria varchar NOT NULL,
+	descripcion varchar NOT NULL,
+	fechacreacion timestamp NOT NULL
+);
+
+CREATE TABLE roles (
+	codigorol SERIAL PRIMARY KEY,
+	rol varchar NOT NULL,
+	descripcion varchar NOT NULL,
+	fechacreacion timestamp NOT NULL
+);
+
+CREATE TABLE usuarios (
+	nombre varchar NOT NULL,
+	apellido varchar NOT NULL,
+	username varchar NOT NULL PRIMARY KEY,
+	password varchar NOT NULL,
+	codigorol int NOT NULL,
+	fechacreacion timestamp NOT NULL,
+	fechamodificacion timestamp NOT NULL,
+	FOREIGN KEY (codigorol) REFERENCES roles(codigorol)
+);
+
 CREATE TABLE productos (
 	producto varchar NOT NULL,
 	codigoproducto varchar NOT NULL PRIMARY KEY,
 	descripcion varchar NULL,
-	precio float4 NOT NULL,
+	precio NUMERIC(10, 2) NOT NULL,
 	cantidad int NOT NULL,
 	activo boolean NOT NULL,
 	codigocategoria varchar NOT NULL,
@@ -12,34 +36,19 @@ CREATE TABLE productos (
 	FOREIGN KEY (codigocategoria) REFERENCES categorias(codigocategoria)
 );
 
---tabla categoría de producto script:
-CREATE TABLE categorias (
-	codigocategoria varchar NOT NULL PRIMARY KEY,
-	categoria varchar NOT NULL,
-	descripcion varchar NOT NULL,
-	fechacreacion timestamp NOT NULL,
-);
-
---tabla movimientos script:
-
 CREATE TABLE ordenes (
-	tipomovimiento varchar NOT NULL,
-	cantidad int NOT NULL,
-	fechamovimiento timestamp NOT NULL,
-	username varchar NULL,
-	codigoproducto varchar NULL,
-	CONSTRAINT usuario FOREIGN KEY (username) REFERENCES usuarios(username),
-	CONSTRAINT codigoproducto FOREIGN KEY (codigoproducto) REFERENCES productos(codigoproducto)
+	codigoorden SERIAL PRIMARY KEY,
+	fechacreacion timestamp NOT NULL,
+	responsable varchar NOT NULL,
+	FOREIGN KEY (responsable) REFERENCES usuarios(username)
 );
 
---tabla usuario script:
-CREATE TABLE usuarios (
-	nombre varchar NOT NULL,
-	apellido varchar NOT NULL,
-	username varchar NOT NULL,
-	"password" varchar NOT NULL,
-	rol varchar NOT NULL,
-	fechacreacion timestamp NOT NULL,
-	fechamodificacion timestamp NOT NULL,
-	CONSTRAINT usuarios_pk PRIMARY KEY (username)
+CREATE TABLE detalleordenes (
+	codigodetalleorden SERIAL PRIMARY KEY,
+	codigoorden int NOT NULL,
+	codigoproducto varchar NOT NULL,
+	cantidad int NOt NULL,
+	precio NUMERIC(10, 2) NOT NULL,
+	FOREIGN KEY (codigoorden) REFERENCES ordenes(codigoorden),
+	FOREIGN KEY (codigoproducto) REFERENCES productos(codigoproducto)
 );
