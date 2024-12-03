0d625a305084d2e878dc27ab4ff0a6305af48481
kevinjvb27 <141968776+kevinjvb27@users.noreply.github.com>
2024-12-02 21:24:27 -0500

    Update DBInventorySystem.sql

--- DBInventorySystem.sql
+++ DBInventorySystem.sql
@@ -1,7 +1,5 @@
-
 --tabla producto script:
-CREATE TABLE public.productos (
-	id int NULL,
+CREATE TABLE productos (
 	producto varchar NOT NULL,
 	codigoproducto varchar NOT NULL,
 	descripcion varchar NULL,
@@ -10,23 +8,11 @@ CREATE TABLE public.productos (
 	fechacreacion timestamp NOT NULL,
 	modificacion timestamp NOT NULL,
 	CONSTRAINT productos_pk PRIMARY KEY (codigoproducto),
-	CONSTRAINT productos_categorias_fk FOREIGN KEY (codigocategoria) REFERENCES public.categorias(codigocategoria)
+	CONSTRAINT productos_categorias_fk FOREIGN KEY (codigocategoria) REFERENCES categorias(codigocategoria)
 );
 
--- Column comments
-
-COMMENT ON COLUMN public.productos.id IS 'id de producto';
-COMMENT ON COLUMN public.productos.producto IS 'Nombre del producto';
-COMMENT ON COLUMN public.productos.codigoproducto IS 'Codigo identificador unico del producto';
-COMMENT ON COLUMN public.productos.descripcion IS 'Descripcion del producto';
-COMMENT ON COLUMN public.productos.precio IS 'Precio del producto';
-COMMENT ON COLUMN public.productos.cantidad IS 'Cantidad de stock del producto';
-
-
-
 --tabla categoría de producto script:
-CREATE TABLE public.categorias (
-	id int NOT NULL,
+CREATE TABLE categorias (
 	categoria varchar NOT NULL,
 	codigocategoria varchar NOT NULL,
 	descripcion varchar NOT NULL,
@@ -34,40 +20,20 @@ CREATE TABLE public.categorias (
 	CONSTRAINT categorias_pk PRIMARY KEY (codigocategoria)
 );
 
--- Column comments
-
-COMMENT ON COLUMN public.categorias.id IS 'id de la categoria';
-COMMENT ON COLUMN public.categorias.categoria IS 'Categoria de los productos';
-COMMENT ON COLUMN public.categorias.codigocategoria IS 'Codigo unico para cada categoria de producto';
-COMMENT ON COLUMN public.categorias.descripcion IS 'Descripcion de cada categoria';
-COMMENT ON COLUMN public.categorias.fechacreacion IS 'Fecha de creacion de la categoria';
-
-
 --tabla movimientos script:
 
-CREATE TABLE public.movimientos (
-	id int NOT NULL,
+CREATE TABLE ordenes (
 	tipomovimiento varchar NOT NULL,
 	cantidad int NOT NULL,
 	fechamovimiento timestamp NOT NULL,
 	username varchar NULL,
 	codigoproducto varchar NULL,
-	CONSTRAINT usuario FOREIGN KEY (username) REFERENCES public.usuarios(username),
-	CONSTRAINT codigoproducto FOREIGN KEY (codigoproducto) REFERENCES public.productos(codigoproducto)
+	CONSTRAINT usuario FOREIGN KEY (username) REFERENCES usuarios(username),
+	CONSTRAINT codigoproducto FOREIGN KEY (codigoproducto) REFERENCES productos(codigoproducto)
 );
 
--- Column comments
-
-COMMENT ON COLUMN public.movimientos.id IS 'id de los movimientos de producto';
-COMMENT ON COLUMN public.movimientos.tipomovimiento IS 'Muestra el tipo de movimiento entrada o salida';
-COMMENT ON COLUMN public.movimientos.cantidad IS 'Muestra la cantidad añadidad o retirada';
-COMMENT ON COLUMN public.movimientos.fechamovimiento IS 'Fecha de movimiento de producto entrada o salida';
-
-
-
 --tabla usuario script:
-CREATE TABLE public.usuarios (
-	id int NOT NULL,
+CREATE TABLE usuarios (
 	nombre varchar NOT NULL,
 	apellido varchar NOT NULL,
 	username varchar NOT NULL,
@@ -77,16 +43,3 @@ CREATE TABLE public.usuarios (
 	fechamodificacion timestamp NOT NULL,
 	CONSTRAINT usuarios_pk PRIMARY KEY (username)
 );
-
--- Column comments
-
-COMMENT ON COLUMN public.usuarios.id IS 'id para usuarios en tabla';
-COMMENT ON COLUMN public.usuarios.nombre IS 'Nombre del usuario';
-COMMENT ON COLUMN public.usuarios.apellido IS 'apellido del usuario';
-COMMENT ON COLUMN public.usuarios.username IS 'nombre de usuario del usuario';
-COMMENT ON COLUMN public.usuarios."password" IS 'contraseña del usuario';
-COMMENT ON COLUMN public.usuarios.rol IS 'rol para cada usuario';
-COMMENT ON COLUMN public.usuarios.fechacreacion IS 'fecha de creacion del usuario';
-COMMENT ON COLUMN public.usuarios.fechamodificacion IS 'fecha de modificacion del usuario';
-
-
