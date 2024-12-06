7f8b9dac544ddeffcfae4b52670d19f97af26561
Alexander Castaño Duque <alexcasdu@hotmail.com>
2024-12-05 21:35:21 -0500

    feat: Se adicciona dependencia de PasswordEnconder

--- src/main/java/com/kevinvelasquez/InventorySystem/service/UsuarioService.java
+++ src/main/java/com/kevinvelasquez/InventorySystem/service/UsuarioService.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Service
 public class UsuarioService {
