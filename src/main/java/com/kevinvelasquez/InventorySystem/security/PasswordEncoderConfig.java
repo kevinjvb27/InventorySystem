a47edfb6ca4699ee9fbc5b17bbb2637f346cc0c1
Alexander Castano <42116081+AlexCasdu@users.noreply.github.com>
2024-12-10 09:11:08 -0500

    add CORS

--- src/main/java/com/kevinvelasquez/InventorySystem/security/PasswordEncoderConfig.java
+++ src/main/java/com/kevinvelasquez/InventorySystem/security/PasswordEncoderConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class PasswordEncoderConfig {
@@ -29,5 +31,17 @@ public class PasswordEncoderConfig {
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
     }
 }
